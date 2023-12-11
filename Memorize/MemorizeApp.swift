1ec302c8bae1f268ccbf45e5eb446e8ee630853e
TarTheesaran <tar.theesaran@odds.team>
2023-12-11 14:16:35 +0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Tar on 11/12/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

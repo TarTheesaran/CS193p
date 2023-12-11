414878a723beabcd824cb0bed841b6c79cc15fe4
TarTheesaran <tar.theesaran@odds.team>
2023-12-11 21:41:06 +0700

    Add 4 ZStack to HStack for display item list

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack(content: {
-            RoundedRectangle(cornerRadius: 12)
-                .foregroundColor(.white)
-            RoundedRectangle(cornerRadius: 12)
-                .strokeBorder(lineWidth:2)
-            Text("👻").font(.largeTitle)
-        })
+        HStack{
+            ZStack(content: {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth:2)
+                Text("👻").font(.largeTitle)
+            })
+            ZStack(content: {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth:2)
+                Text("👻").font(.largeTitle)
+            })
+            ZStack(content: {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth:2)
+                Text("👻").font(.largeTitle)
+            })
+            ZStack(content: {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth:2)
+                Text("👻").font(.largeTitle)
+            })
+        }
         .foregroundStyle(.orange)
         .padding()
     }

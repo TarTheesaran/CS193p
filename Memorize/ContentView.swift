7083d1a9fdbef127a34aa0007dd07b150cf75f33
TarTheesaran <tar.theesaran@odds.team>
2024-01-02 23:36:50 +0700

    Adjust CardView layout

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -15,25 +15,30 @@ struct ContentView: View {
     var body: some View {
         VStack {
             cards
-            HStack {
-                cardRemover
-                Spacer()
-                cardAdder
-            }
-            .imageScale(.large)
-            .font(.largeTitle)
+            Spacer()
+            cardCountAdjuster
         }
         .padding()
     }
     
     var cards: some View {
-        HStack {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
             ForEach(0..<cardCount, id: \.self) { index in
                 CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
             }
-            
         }
         .foregroundStyle(.orange)
+    }
+    
+    var cardCountAdjuster: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
     }
     
     func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
@@ -60,13 +65,13 @@ struct CardView: View {
     var body: some View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: 12)
-            if(isFaceUp){
+            Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
                 Text(content).font(.largeTitle)
-            } else{
-                base.fill()
             }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
         }
         .onTapGesture {
             isFaceUp.toggle()
